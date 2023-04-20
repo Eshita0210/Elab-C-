250c48fa9cf3f3fe74283f899bb432cd3e9cdf20
Eshita Mishra <86978758+Eshita0210@users.noreply.github.com>
2023-04-20 08:51:13 +0530

    Create Raja & John.cpp

---
+++ Raja & John.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    int a,b;
+    cin>>a>>b;
+    cout<<__gcd(b,a);
+
+    return 0;
+    cout<<"Event operator+ (Event obj) class Event Event obj1; return obj1;";
+    
+}
