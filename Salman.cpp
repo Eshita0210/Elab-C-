51ac604f365e91a62ab8ffd6c450462ea59a84d8
Eshita Mishra <86978758+Eshita0210@users.noreply.github.com>
2023-04-20 08:52:15 +0530

    Create Salman.cpp

---
+++ Salman.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void graaw(){
+    cout<<"class Student class Test :public Student class Result :public Test Result r; r.print();  r.accept(); r.check();";
+}
+int main()
+{int a;
+cin>>a;
+if(a>60)
+cout<<"You have passed";
+else
+cout<<"You have failed";
+
+	return 0;
+}
