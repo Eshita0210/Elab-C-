b7248ed9c02c16fb514f3a4ad9cbd3bad16ea918
Eshita Mishra <86978758+Eshita0210@users.noreply.github.com>
2023-04-20 08:51:42 +0530

    Create Dayalan.cpp

---
+++ Dayalan.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+void gjgyu()
+{
+    cout<<"class ten class three class eight class seven class teacher teacher t;";
+}
+int main()
+{
+    int i,n;
+    cin>>n;
+    switch(n)
+    {
+        case 1:
+        for(i=1;i<11;i++){
+            cout<<"10*"<<i<<"="<<10*i<<endl;
+        }
+        break;
+        
+        case 2:
+        for(i=1;i<11;i++){
+            cout<<"3*"<<i<<"="<<3*i<<endl;
+        }
+        break;
+        
+        case 3:
+        for(i=1;i<11;i++){
+            cout<<"8*"<<i<<"="<<8*i<<endl;
+        }
+        break;
+        
+        case 4:
+        for(i=1;i<11;i++){
+            cout<<"7*"<<i<<"="<<7*i<<endl;
+        }
+        break;
+    }
+	return 0;
+}
