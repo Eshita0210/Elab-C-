a89295e627918f6fc97a4e8140cf13bddd204ed9
Eshita Mishra <86978758+Eshita0210@users.noreply.github.com>
2023-04-24 10:56:19 +0530

    Create Hameed and Zahir.cpp

---
+++ Hameed and Zahir.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+template <class T>
+void InterchangeFavPlayers(T &player1,T &player2){
+    cout<<player2<<" "<<player1;
+}
+int main()
+{
+    string player1,player2;
+    cin>>player1>>player2;
+    InterchangeFavPlayers(player1,player2);
+	return 0;
+}
