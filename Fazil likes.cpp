58c1f4cc3fde663b5073c5e2f84ad4c69be94499
Eshita Mishra <86978758+Eshita0210@users.noreply.github.com>
2023-04-24 10:34:20 +0530

    Create Fazil likes.cpp

---
+++ Fazil likes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#define s string
+class Tea{
+ public:virtual void Cup()=0;
+};
+class Drink:public Tea{
+ public:
+ void Cup(){
+ }
+};
+int main(){
+ Drink obj;
+ obj.Cup();
+ int n,k,a,b,z,i;
+cin>>n>>k>>a>>b;
+s r = "";
+char x='G',y='B';
+if(a<b)
+swap(a,b),swap(x,y);
+z=(a-1)/k+1;
+if(z>b+1)
+return cout<<"NO", 0;
+for(i=0;i<z-1;i++)
+r+=s(k,x)+s(b/z+(i<b%z?1:0),y);
+r+=s(a-k*(z-1),x)+s(b/z,y);
+cout<<r;
+}
