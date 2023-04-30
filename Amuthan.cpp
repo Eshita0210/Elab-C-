6131356d6b5e046a95223b2d765725f2c03c6e65
Eshita Mishra <86978758+Eshita0210@users.noreply.github.com>
2023-04-30 23:34:40 +0530

    Create Amuthan.cpp

---
+++ Amuthan.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int donuts,milk;
+    try{
+        cin>>donuts;
+        cin>>milk;
+        if(milk==0)
+        throw donuts;
+        else
+        cout<<"You have "<<(float)donuts/milk<<" donuts for each glass of milk";
+    }
+    catch(int e){
+        cout<<e<<" donuts and No Milk\nGo buy some milk";
+    }
+	return 0;
+}
